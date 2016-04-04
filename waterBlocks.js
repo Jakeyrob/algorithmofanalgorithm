65511a24aa2a356aa2617aa0471445f49756eb9c
Jake Pace <jakeyrob@gmail.com>
2016-04-04 14:51:53 -0700

    Implement waterBlocks.js

--- waterBlocks.js
+++ waterBlocks.js
@@ -22,7 +22,45 @@
 /////////////////////////////////////////////////////////////
 
 var waterBlocks = function (blocks) {
-  // TODO: Implement
+  // deduct number of blocks in current row from smallest on left or right
+
+  // Store water count
+  var water = 0;
+
+  // Store highest tower left
+  var highestLeft = 0;
+
+  // store highest tower right
+  var highestRight = 0;
+
+  // Iterate over array
+  for (var i = 0; i < blocks.length; i++) {
+
+    // if current larger than highest, replace highest
+    if (blocks[i] > highestLeft) {
+      highestLeft = blocks[i];
+
+    // if current is less than highest, iterate over remaining blocks to check if any are higher
+    } else if (blocks[i] < highestLeft) {
+      for (var j = i; j < blocks.length; j++) {
+        if (blocks[j] > highestRight && blocks[j] > highestLeft) {
+          highestRight = blocks[j];
+          console.log("highestRight changed: ", highestRight);
+          break;
+        }
+        else if (blocks[j] > highestRight) {
+          highestRight = blocks[j];
+          console.log("highestRight changed: ", highestRight);
+        }
+      }
+      var newWater = Math.min(highestLeft, highestRight) - blocks[i];
+      console.log("newWater is: ", newWater);
+      water += newWater;
+      highestRight = 0;
+    }
+  }
+
+  return water;
 };
 
 //////////////////////////////////////////////////
