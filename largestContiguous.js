1a7291987384cd799798b9489f41bc2342468c9f
Jake Pace <jakeyrob@gmail.com>
2016-04-04 15:22:38 -0700

    Implement largestContiguous.js

--- largestContiguous.js
+++ largestContiguous.js
@@ -11,9 +11,38 @@
 /////////////////////////////////////////////////////////////
 
 
-
 var largestContiguousSum = function (array) {
-  // TODO: Implement
+  // store highestSum
+  var highestSum = 0;
+
+  // store currentSum
+  var currentSum = 0;
+
+  // iterate over array
+  for (var i = 0; i < array.length; i++) {
+    
+    // if current >= 0, add to sum
+    if (array[i] >= 0) {
+      currentSum += array[i];
+    } else {
+
+      // if currentSum + current < 0, currentSum = 0
+      if (currentSum + array[i] < 0) {
+        currentSum = 0;
+      } else {
+        // currentSum += current
+        currentSum += array[i];
+      }
+    }
+
+    // if currentSum > highestSum, highestSum = currentSum
+    if (currentSum > highestSum) {
+      highestSum = currentSum;
+    }
+  }
+
+  // return highestSum
+  return highestSum;
 };
 
 
